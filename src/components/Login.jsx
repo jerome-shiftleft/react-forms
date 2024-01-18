9eda9ce644c4ff82f8767edc45b9574ee6f33bb0
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-19 03:27:17 +0800

    progress

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
@@ -8,20 +9,30 @@ export default function Login() {
     event.preventDefault();
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+    
     console.log(`
       email: ${enteredEmail}
       password: ${enteredPassword}
     `);
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
+      {/* noValidate disable default browser's validation */}
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email.</p>}</div>
         </div>
 
         <div className="control no-margin">
