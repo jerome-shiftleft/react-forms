a97cbda1fa5d5241bfe87bc5cc83ed4f1307b789
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-19 01:27:41 +0800

    progress

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,7 +8,12 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -19,6 +24,13 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -33,10 +45,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
-          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
