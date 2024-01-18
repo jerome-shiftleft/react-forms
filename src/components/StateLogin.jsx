4074b26cde73e931363794dda56abe4ac50f10d5
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-19 01:15:59 +0800

    progress

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,8 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -34,6 +36,7 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
